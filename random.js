b65c0eec84aa197439ecbaa3a251eb37c37dba40
abhinav215 <66450065+abhinav215@users.noreply.github.com>
2022-05-20 23:07:29 +0530

    Delete random.js

--- random.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  var parent = $("#cards");
-  var divs = parent.children();
-  while (divs.length) {
-      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-  }
-});
